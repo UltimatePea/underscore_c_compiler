5b2c046e4005f05b520a121f67c9bd1bb5fb1615
UltimatePea <superchenzb@gmail.com>
2017-03-12 01:15:53 -0800

    Update Alogrithm and Add Read Me

--- compile.hs
+++ compile.hs
@@ -29,7 +29,13 @@ processLines tokens lines = let underscoreLized = L.map (processLine tokens) lin
 defineTokens :: [Token] -> [String]
 defineTokens xs = L.map getDefineForTag xs
                   where getDefineForTag :: Token -> String
-                        getDefineForTag tk = "#define " ++ replicate (index tk+1) '_' ++ " " ++ getTokenContent tk
+                        -- only define when token is valid id
+                        getDefineForTag tk 
+                            | isTokenValidID tk = "#define " 
+                                ++ replicate (index tk+1) '_' 
+                                ++ " " 
+                                ++ getTokenContent tk
+                            | otherwise = []
                         index tk = case tk `elemIndex` xs of 
                             Just idx -> idx
                             Nothing -> error "Global Define :: Token Not Found"
@@ -54,32 +60,54 @@ mapToken :: [Token] -- global token table
            -> Token -- a particular token to match
            -> String -- the string representation of the token, underscores
 mapToken tokens tk = case tk `elemIndex` tokens of
-                        Just idx -> ' ' : replicate (idx+1) '_'
+                        Just idx -> if isTokenValidID tk  -- only substitute when token is underscore
+                                    then ' ' : replicate (idx+1) '_' ++ " " -- is id, put space and underscore
+                                    else getTokenContent tk -- not token, just put the original string
                         Nothing -> error "Token Not Found, Check implementation logic"
 
 
-data Token = Token String deriving (Show, Ord, Eq)
+data Token = Token 
+                    String -- string content
+                    Bool  -- True if the token is valid ID([a-zA-Z0-9], false otherwise
+                    deriving (Show, Ord, Eq)
 
 -- parses and gets the token content that is compatible with C Macro
 getTokenContent :: Token -> String
-getTokenContent (Token s) = if length s /= 1 
-                            then s 
-                            else
-                                case head s of
-                                    '{' -> "<%"
-                                    '}' -> "%>"
-                                    '[' -> "<:"
-                                    ']' -> ":>"
-                                    '#' -> "%:"
-                                    _ -> s
+getTokenContent (Token s _) = s
+isTokenValidID :: Token -> Bool
+isTokenValidID (Token _ b) = b
+--getTokenContent (Token s) = if length s /= 1 
+--                            then s 
+--                            else
+--                                case head s of
+--                                    '{' -> "<%"
+--                                    '}' -> "%>"
+--                                    '[' -> "<:"
+--                                    ']' -> ":>"
+--                                    '#' -> "%:"
+--                                    _ -> s
 
 
 tokenize :: String -> [Token]
 tokenize [] = []
 tokenize (x:xs)
-    | isValidID x = Token (x:(takeWhile isValidID xs)) : tokenize (dropWhile isValidID xs)
+    | isValidID x = Token (x:(takeWhile isValidID xs)) True: tokenize (dropWhile isValidID xs)
     | isSpace x = tokenize xs
-    | otherwise = Token [x] : tokenize xs
+    | isQuote x = Token (x:(fst $ quoteEscape xs)) False : tokenize ( snd $ quoteEscape xs)
+    | otherwise = Token [x] False: tokenize xs
 
 isValidID :: Char -> Bool
 isValidID x = (isAlphaNum x ) || ( x == '_')
+
+isQuote :: Char -> Bool
+isQuote x = x == '\"'
+
+--quote escape :: hel, fjis\"jisjv"sub -> (hel, fjis\"jisjv", sub)
+quoteEscape :: String -> (String, String)
+quoteEscape str = quoteEscapeRec ("", str) 
+                  where quoteEscapeRec :: (String, String) -> (String, String)
+                        --single escape
+                        quoteEscapeRec (a,('\\':x:xs)) = quoteEscapeRec (a ++ ['\\'] ++ [x], xs)
+                        quoteEscapeRec (a,('\"':xs)) = (a ++ "\"", xs)
+                        quoteEscapeRec (a,(x:xs)) = quoteEscapeRec (a++[x], xs)
+                        quoteEscapeRec (a, [] ) = (a, [])
